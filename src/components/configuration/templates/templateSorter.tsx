300d4262f9336bab3af298984edf20b6a2abbd3d
MikkoHolfeld <Bitch-11>
2023-07-27 15:50:38 +0200

    add: templateSorting

--- src/components/configuration/templates/templateSorter.tsx
+++ src/components/configuration/templates/templateSorter.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import ViewListIcon from '@mui/icons-material/ViewList';
-import ViewQuiltIcon from '@mui/icons-material/ViewQuilt';
-import TableRowsIcon from '@mui/icons-material/TableRows';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import {useSelector} from "react-redux";
--- src/enums/teplateSorting.enum.ts
+++ src/enums/teplateSorting.enum.ts
@@ -1,5 +1,8 @@
 export enum TEMPLATE_SORTING {
     ALPHABETICALLY = 'alphabetically',
+    ALPHABETICALLY_REVERSE = 'alphabeticallyReverse',
     BY_DATE = 'byDate',
+    BY_DATE_REVERSE = 'byDateReverse',
     BY_TYPE = 'byType',
+    BY_TYPE_REVERSE = 'byTypeReverse'
 }
